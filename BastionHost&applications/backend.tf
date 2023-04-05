d1294f27194e99cfe5cc72169ea7a54d9fe8ed48
Udayeswari savana <98032403+udayayeswar@users.noreply.github.com>
2023-04-05 10:02:54 -0400

    Update backend.tf

--- BastionHost&applications/backend.tf
+++ BastionHost&applications/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "udaya-terraform-state-backend"
     key            = "state/apps/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
